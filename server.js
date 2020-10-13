b2b0111bb53befeb88eea5db651ee671046b4a54
Mark Bell <skrambelled@gmail.com>
2020-10-12 20:53:17 -0700

    got locations to work

--- server.js
+++ server.js
@@ -3,16 +3,37 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
+const cors = require('cors');
 
 const PORT = process.env.PORT;
 
-app.use(cors());
-
 app.get('/', (req, res) => {
   res.send('hello world');
+});
+
+app.use(cors());
+
+function Location(city, geoData) {
+  this.search_query = city;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
+  this.formatted_query = geoData[0].display_name;
+}
+
+app.get('/location', (req, res) => {
+  try {
+    const geoData = require('./data/location.json');
+    const city = req.query.city;
+    const locationData = new Location(city, geoData);
+    res.json(locationData);
+  } catch (error) {
+    res.status(500).send('Error');
+  }
+});
+
+app.get('/weather', (req, res) => {
+
 });
 
 // default 404 error handling
